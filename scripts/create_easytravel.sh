5e417966f9b5991d0e5962d7904995bdc5dab790
Agyl Riyan Pradipta <agylpradipta@Agyls-MacBook-Air.local>
2023-07-16 01:30:40 +0700

    revised angular

--- scripts/create_easytravel.sh
+++ scripts/create_easytravel.sh
@@ -5,4 +5,5 @@ kubectl apply -f ../manifests/easytravel-config.yaml
 kubectl apply -f ../manifests/easytravel-mongodb.yaml
 kubectl apply -f ../manifests/easytravel-backend.yaml
 kubectl apply -f ../manifests/easytravel-frontend.yaml
+kubectl apply -f ../manifests/easytravel-angular-frontend.yaml
 kubectl apply -f ../manifests/easytravel-nginx.yaml
